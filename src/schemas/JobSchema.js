0816973a630aeb3e0f6c448d734b477336077b86
niyi-16 <dasil.adam@gmail.com>
2026-03-16 00:29:06 -0300

    Modified some messaged

--- src/schemas/JobSchema.js
+++ src/schemas/JobSchema.js
@@ -4,11 +4,11 @@ export const JobSchema = z.object({
     id: z.union([z.string(), z.number()]).optional(),
 
     title: z.string()
-        .min(3, "Surely it's not that short")
+        .min(3, "Surely the job has a title, right?")
         .max(100, "Title is too long"),
 
     company: z.string()
-        .min(3, "Surely it's not that short")
+        .min(3, "Surely The company has a name and it's not this short, right?")
         .max(100, "Company is too long"),
 
     status: z.enum(["Applied", "Interview", "Offer", "Rejected"]),
@@ -17,7 +17,7 @@ export const JobSchema = z.object({
         .max(1000, "Notes are too long")
         .optional(),
 
-    jobUrl: z.url("Surely it's not that short")
+    jobUrl: z.url("Surely there's a link to the job?")
         .max(500, "URL is too long")
         .optional(),
 
@@ -25,5 +25,5 @@ export const JobSchema = z.object({
     updatedAt: z.string().optional(),
 });
 
-export const CreateJobSchema = JobSchema.omit({id: true, updatedAt: true});
-export const UpdateJobSchema = JobSchema.omit({updatedAt: true});
+export const CreateJobSchema = JobSchema.omit({id: true, updatedAt: true, appliedAt: true});
+export const UpdateJobSchema = JobSchema.omit({updatedAt: true, notes: true});
